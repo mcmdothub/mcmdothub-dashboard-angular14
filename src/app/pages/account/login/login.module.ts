d9f39e9151d614a2efebdaa3df1bf2014549e353
cristianlinear <cristian.marin@linearsystem.it>
2022-11-29 21:16:56 +0100

    Design and Validate login page with BS 5

--- src/app/pages/account/login/login.module.ts
+++ src/app/pages/account/login/login.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -9,10 +10,12 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild([
       {
         path: '',
-        component: LoginComponent
+        component: LoginComponent,
+        title: 'Login page'
       }
     ])
   ]
