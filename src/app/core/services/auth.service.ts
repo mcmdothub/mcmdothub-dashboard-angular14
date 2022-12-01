aa07c46360d9b2d09c3ffa3180ce900339614b72
cristianlinear <cristian.marin@linearsystem.it>
2022-12-01 17:47:54 +0100

    Role Based Authentication

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from "rxjs/operators";
+import { delay, map } from "rxjs/operators";
 import { ILogin } from "../../pages/account/models/login.interface";
 import { IUser } from "../models/user.interface";
 
@@ -15,10 +15,8 @@ export class AuthService {
     return this._user;
   }
 
-  constructor(
-    private _httpClient: HttpClient
-  ) {
-    this._user = { firstName: '', profilePicPath: '', role: '', userCode: ''};
+  constructor(private _httpClient: HttpClient) {
+    this._user = { firstName: '', profilePicPath: '', role: '', userCode: '' };
   }
 
   /*
@@ -42,14 +40,15 @@ export class AuthService {
 
     // returns an Observable with Object
     return this._httpClient.get('https://ytc.beginner2expert.com/angular14/api/public/secure/user/basic/details', headers)
+    .pipe(delay(2000))
     .pipe(map((apiResponse: any) => {
       //apiResponse.data.profilePic = 'https://ytc.beginner2expert.com/angular14/api/' + apiResponse.data.profilePic;
       //return apiResponse.data;
 
       this._user = {
         ...apiResponse.data,
-        profilePicPath: apiResponse.profilePic,
-        role: apiResponse.roleName,                         // when different name in the interface respect what it comes from http response "roleName" => we have to map "role: apiResponse.roleName"
+        profilePicPath: apiResponse.data.profilePic,
+        role: apiResponse.data.roleName,                         // when different name in the interface respect what it comes from http response "roleName" => we have to map "role: apiResponse.roleName"
       };
 
       return this._user;
