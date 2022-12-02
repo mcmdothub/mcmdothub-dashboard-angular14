21ae9e6a10e8ac680594b530dfa2c7857cea0357
cristianlinear <cristian.marin@linearsystem.it>
2022-12-02 16:12:48 +0100

    Table Sorting

--- projects/custom-library/src/lib/components/custom-library/custom-library.component.ts
+++ projects/custom-library/src/lib/components/custom-library/custom-library.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'lib-custom-library',
@@ -9,13 +11,26 @@ export class CustomLibraryComponent implements OnInit, OnChanges {
   @Input('cols') columnList: string[];
   @Input('rows') rowList: any[];
 
+  mtDataSource: MatTableDataSource<any>;
+  @ViewChild(MatSort) sort!: MatSort;
+
   constructor() {
     this.columnList = [];
     this.rowList = [];
+    this.mtDataSource = new MatTableDataSource();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
+    if (changes['rowList']?.currentValue.length) {
+      this.mtDataSource = new MatTableDataSource(this.rowList);
+      this.mtDataSource.sort = this.sort;
+    }
+  }
 
+  sortColumn(sort: Sort) {
+    if(sort.direction) {
+      this.mtDataSource.sort = this.sort;
+    }
   }
 
   ngOnInit(): void {}
